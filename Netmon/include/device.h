61e3a3e0b395c1d1ef2493cfc18fd8d16fdf6c36
Yukari-ユカリ <gnail102g@gmail.com>
2026-03-26 22:09:57 +0100

    Added: Download functionality.

--- Netmon/include/device.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEVICE_H
-#define DEVICE_H
-#include <pcap.h>
-
-pcap_t *open_device(const char* device, const char* filter);
-void close_device(pcap_t *handle);
-
-#endif
--- Netmon/plugins/logger.py
+++
@@ -1,3 +0,0 @@
-def on_packet(src_ip, dst_ip, protocol, size):
-    with open("netmon.log", "a") as f:
-        f.write(f"{src_ip} -> {dst_ip} [{protocol}] {size} bytes\n")
